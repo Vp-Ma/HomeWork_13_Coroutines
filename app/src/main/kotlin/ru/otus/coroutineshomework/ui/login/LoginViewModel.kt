7c63386c4d29636b3193d16facfcd64517cdaef3
Vp-Ma <vagrant18pm@gmail.com>
2026-01-11 10:34:28 +0300

    Task 1.3 implementation ( Login by coroutines )

--- app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
+++ app/src/main/kotlin/ru/otus/coroutineshomework/ui/login/LoginViewModel.kt
@@ -3,11 +3,19 @@ package ru.otus.coroutineshomework.ui.login
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import ru.otus.coroutineshomework.ui.login.data.Credentials
+import ru.otus.coroutineshomework.ui.login.data.User
 
 class LoginViewModel : ViewModel() {
 
     private val _state = MutableLiveData<LoginViewState>(LoginViewState.Login())
     val state: LiveData<LoginViewState> = _state
+    private val loginApi = LoginApi()
+    private lateinit var userInfo: User
 
     /**
      * Login to the network
@@ -15,13 +23,33 @@ class LoginViewModel : ViewModel() {
      * @param password user password
      */
     fun login(name: String, password: String) {
-        // TODO: Implement login
+
+        _state.value = LoginViewState.LoggingIn
+        viewModelScope.launch {
+            try {
+                withContext(Dispatchers.IO) {
+                    userInfo = loginApi.login(Credentials(name, password))
+                }
+                _state.value = LoginViewState.Content(userInfo)
+            } catch (e: IllegalArgumentException) {
+                withContext(Dispatchers.Main) {
+                    _state.value = LoginViewState.Login(e)
+                }
+            }
+        }
     }
 
     /**
      * Logout from the network
      */
     fun logout() {
-        // TODO: Implement logout
+
+        _state.value = LoginViewState.LoggingOut
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                loginApi.logout()
+            }
+            _state.value = LoginViewState.Login()
+        }
     }
 }
